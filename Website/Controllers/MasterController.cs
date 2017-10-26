c68c72ef963a8d995325dc3714563cdac0d18041
GhorghorBey <s.paquay@gmail.com>
2017-10-26 13:01:12 +0200

    Initial commit

---
+++ Website/Controllers/MasterController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Website.Models;
+
+namespace Website.Controllers
+{
+	public abstract class MasterController : Controller
+	{
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ Website/Controllers/ServicesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Website.Models;
+
+namespace Website.Controllers
+{
+	public class ServicesController : MasterController
+	{
+		public IActionResult Index()
+		{
+			ViewData["Message"] = "Your application description page.";
+			return View();
+		}
+	}
+}
